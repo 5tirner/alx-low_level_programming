fc9ef86b9fdee620ddad61941c9a0f76e83c9595
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-15 08:03:37 -0700

    add hints

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * get_len function computes the length of the string s
  *
- * Return: Always 0 (Success)
+ * Return: lenght of the string s
  */
 
 int	get_len(char *s)
 {
+	/* i : integer with size 4byte  use it to know the lenght of the string s by increment it until '\0' */
 	int i;
 
 	i = 0;
@@ -16,9 +17,17 @@ int	get_len(char *s)
 	return (i);
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
+	/* s : pointer to char wit size 8 byte i use it as buffer to the quote */ 
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, s, get_len(s));
 	return (1);
 }
