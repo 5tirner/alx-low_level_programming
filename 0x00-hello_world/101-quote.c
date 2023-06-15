5dc1a31e21063e559adac922d3685258f619e022
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-15 08:07:30 -0700

    betty is bsh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,6 @@
 #include <unistd.h>
 
 /**
- * get_len function computes the length of the string s
- *
- * Return: lenght of the string s
- */
-
-int	get_len(char *s)
-{
-	/* i : integer with size 4byte  use it to know the lenght of the string s by increment it until '\0' */
-	int i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -25,9 +8,6 @@ int	get_len(char *s)
 
 int main(void)
 {
-	/* s : pointer to char wit size 8 byte i use it as buffer to the quote */ 
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, s, get_len(s));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 57);
 	return (1);
 }
