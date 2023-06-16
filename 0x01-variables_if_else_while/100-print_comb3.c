a0fae7f2838867306c8d18259672dc7d4becdf76
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-16 05:21:34 -0700

    comb1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n1;
+	int n2;
+
+	n1 = '0';
+	while (n1 < '9')
+	{
+		n2 = n1 + 1;
+		while (n2 <= '9')
+		{
+			putchar(n1);
+			putchar(n2);
+			if (n1 != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n2++;
+		}
+		n1++;
+	}
+	putchar('\n');
+	return(0);
+}
