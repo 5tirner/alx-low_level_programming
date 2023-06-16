0e0c79f5283af86a4f3babc97c14df8bd0eb0aab
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-16 03:24:29 -0700

    betty and hard code what a boring work

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n % 10 < 6)
 	{
-		printf("Last digit of %d is %d", n, n % 10);
+		printf("Last digit of %d is %d ", n, n % 10);
 		if (n % 10 == 0)
 			printf("and is 0\n");
 		else
