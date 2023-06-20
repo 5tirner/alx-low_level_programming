161834aaa3330bc4c4c72ddd87455237541857ab
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-20 09:26:50 -0700

    2.0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,19 @@
  * Return: Always 0.
  */
 
-int main(void)
+void	print_putchar(char *str)
 {
-	puts("_putchar");
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
+}
+int	main(void)
+{
+	print_putchar("_putchar\n");
 	return (0);
 }
