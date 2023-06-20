9b6232cf4a478a752f90c62ab8f5da739c1eaed7
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-20 12:26:24 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add a to b
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: Always 0.
+ */
+
+int	add(int a, int b)
+{
+	int	save;
+
+	save = a + b;
+	return (save);
+}
