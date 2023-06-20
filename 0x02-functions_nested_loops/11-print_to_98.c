59a8b2b701a08383a877a3bf0c589a1905ef0a5c
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-20 13:52:28 -0700

    putnbr

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,86 @@
+#include "main.h"
+
+/**
+ * print_nbr - print any integer
+ * @n: the integer that i want print it.
+ *
+ * Return: no return value.
+ */
+
+void	print_nbr(int n)
+{
+	if (n >= 0 && n < 10)
+		_putchar(n + 48);
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_nbr(n * -1);
+	}
+	else if (n >= 10)
+	{
+		print_nbr(n / 10);
+		print_nbr(n % 10);
+	}
+}
+
+/**
+ * case_bigger - from n to 98
+ * @n: from it
+ *
+ * Return: no return value.
+ */
+
+void	case_bigger(int n)
+{
+	int	i;
+
+	i = 0;
+	while (n >= 98)
+	{
+		if (i != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		print_nbr(n);
+		n--;
+		i++;
+	}
+}
+
+void	case_smaller(int n)
+{
+	int	i;
+
+	i = 0;
+	while (n <= 98)
+	{
+		if (i != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		print_nbr(n);
+		n++;
+		i++;
+	}
+}
+
+/**
+ * print_to_98 - print from anything to 98
+ * @n: the elements that i will print from it.
+ *
+ * Return: no return value.
+ */
+
+void	print_to_98(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n > 98)
+		case_bigger(n);
+	else
+		case_smaller(n);
+	_putchar('\n');
+}
