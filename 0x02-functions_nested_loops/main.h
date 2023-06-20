e49029c978f6ffdc800358b40d49ee17e69c41da
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-20 10:35:33 -0700

    abs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,12 +3,14 @@
 
 # include <unistd.h>
 # include <stdio.h>
+
 void	print_alphabet(void);
 void	print_alphabet_x10(void);
 int	_islower(int c);
 void	_putchar(int c);
 int	_isalpha(int c);
 int	print_sign(int n);
+int	_abs(int n);
 
 #endif
 
