af723e518023132b8a6fc09debaaf65f307e447d
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-06-22 14:23:21 -0700

    almost all

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all the digit except 2 & 4
+ *
+ * Return: no return value.
+ */
+
+void	print_most_numbers(void)
+{
+	write(1, "01356789\n", 8);
+}
