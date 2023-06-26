928965b6b1f93ef53d851c975281b28e31dfb59a
Zakaria <117547737+5tirner@users.noreply.github.com>
2023-06-26 20:04:28 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - convert a number to 98
+ * @n: integer that will convert it's val
+ *
+ * Return: no return value.
+ */
+
 void  reset_to_98(int *n)
 {
   *n = 98;
