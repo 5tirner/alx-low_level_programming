538df883f985f4303fdc1c7bf9e2ae1c33690797
5tirner <thatfor1337@gmail.com>
2023-06-26 21:46:03 +0100

    revprint

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * len - count len of s
+ * @s: string to count it's len
+ *
+ * Return: len of s
+ */
+
+int	len(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+/**
  * print_rev - rev string
  * @s: string that i want reverse
  *
@@ -11,7 +27,7 @@ void	print_rev(char *s)
 {
 	int	i;
 
-	i = _strlen(s) - 1;
+	i = len(s) - 1;
 	while (i >= 0)
 	{
 		write(1, &s[i], 1);
