09b41e48f90fa472cd1741b8b5fd6b106eaeecd4
5tirner <thatfor1337@gmail.com>
2023-06-29 18:13:27 +0100

    cmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * s1: first one
+ * s2: second
+ *
+ * Rteurn - difference between the two
+ */
+
+int	_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
