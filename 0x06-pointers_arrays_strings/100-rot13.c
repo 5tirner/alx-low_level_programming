38ac1c45cecf0842feefe96194730969ee910ea4
5tirner <thatfor1337@gmail.com>
2023-07-01 00:13:44 +0100

    try

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot_13 - +13 or =13
+ * @s: string
+ *
+ * Return: pointer to char
+ */
+
+char	*rot_13(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if ((s[i] >= 'a' && s[i] <= 'm')
+			|| (s[i] >= 'A' && s[i] <= 'M'))
+			s[i] += 13;
+		else if ((s[i] >= 'n' && s[i] <= 'z')
+			|| (s[i] >= 'N' && s[i] <= 'Z'))
+			s[i] -= 13;
+		i++;
+	 }
+	return (s);
+}
