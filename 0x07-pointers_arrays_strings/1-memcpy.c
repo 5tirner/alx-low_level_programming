f3bfb16945ca64a2cf3157b638ff2186c31b0d3e
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-03 07:36:55 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy src at dest until n
+ * @dest: destination
+ * @src: source
+ * @n: unsigned int
+ *
+ * Return: Dest
+ */
+
+char	*_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,9 @@
 #include <stdio.h>
 
 char	*_memset(char *s, char b, unsigned int n);
+char	*_memcpy(char *dest, char *src, unsigned int n);
+
+
+
 
 #endif
