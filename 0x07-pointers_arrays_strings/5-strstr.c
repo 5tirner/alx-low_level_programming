2b0ae7cb384037a0940814f83b84a35e0278e0c5
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-03 09:25:46 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - search for needle
+ * @haystack: string1
+ * @needle: string2
+ *
+ * Return: pointer to char or 0
+ */
+
+char	*_strstr(char *haystack, char *needle)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!needle || !needle[0])
+		return (0);
+	while (haystack[i])
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] && haystack[i + j] == needle[j])
+				j++;
+			if (!needle[j])
+				return (haystack + i);
+		}
+		i++;
+	}
+	return (0);
+}
