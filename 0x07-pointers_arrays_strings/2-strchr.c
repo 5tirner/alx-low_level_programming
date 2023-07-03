7bd1f71a47064baf0df62eb9d2f583208de9e9b2
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-03 07:51:06 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * strchr - search for a char
+ * @s: string
+ * @c: char to search for it
+ *
+ * Return: NULL or an address if found c 
+ */
+
+char	*_strchr(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	if (s[i] == c)
+		return (&s[i]);
+	return (0);
+}
