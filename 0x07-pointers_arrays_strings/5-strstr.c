4d394d26f1f0f69e6883f77b9c4e8a515dcca7f6
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-03 10:07:13 -0700

    test

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,8 @@ char	*_strstr(char *haystack, char *needle)
 		j = 0;
 		if (haystack[i] == needle[j])
 		{
-			while (haystack[i + j] && haystack[i + j] == needle[j])
+			while (haystack[i + j]
+			&& haystack[i + j] == needle[j])
 				j++;
 			if (!needle[j])
 				return (haystack + i);
