b03229b23e11b26545fecd105311fbf5cb082254
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-05 15:49:03 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,14 +9,10 @@
 
 int	factorial(int n)
 {
-	int	res;
-
-	res = 1;
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
 	else
-		res = n * factorial(n - 1);
-	return (res);
+		return (n * factorial(n - 1));
 }
