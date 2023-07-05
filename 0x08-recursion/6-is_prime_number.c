49e28fbec8bfd53d76db76489591c869f2282654
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-05 16:51:47 -0700

    isprime with rec

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,62 @@
 #include "main.h"
 
 /**
+ * square_root - check if there is a sqrt
+ * @n: our number
+ * @checker: checker to compare it with n each time
+ *
+ * Return: integer
+ */
+
+int square_root(int n, int checker)
+{
+	int square;
+
+	square = checker * checker;
+	if (square == n)
+		return (checker);
+	else if (square < n)
+		return (square_root(n, 1 + checker));
+	else
+		return (-1);
+}
+
+/**
+ * _sqt - sqrt with rec
+ * @n: calcul it's sqrt
+ *
+ * Return: square root of n
+ */
+
+int	_sqt(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else
+		return (square_root(n, 1));
+}
+
+/**
+ * rec_prime - check if n divided by other nb
+ * @n: number to check
+ * @div: use it each time to check if n can divided
+ *
+ * Return: 1 if cannot divided 0 otherwise
+ */
+
+int	rec_prime(int n, int div)
+{
+	if (n % div == 0)
+		return (0);
+	else if (_sqt(n) < div)
+		return (1);
+	else
+		return (rec_prime(n, div + 1));
+}
+
+/**
  * is_prime_number - check if n is prime
  * @n: nb to check it
  *
@@ -9,21 +65,10 @@
 
 int	is_prime_number(int n)
 {
-	int	i;
-
-	i = n / 2;
 	if (n == 2)
 		return (1);
 	else if (n <= 1 || n % 2 == 0)
 		return (0);
 	else
-	{
-		while (i >= 3)
-		{
-			if (n % i == 0)
-				return (0);
-			i--;
-		}
-	}
-	return (1);
+		return (rec_prime(n, 3));
 }
