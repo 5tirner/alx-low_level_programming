503f138c8e6e9aa1fa25e670f9ab80b2ccba7d80
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-09 23:08:39 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - create a copy
+ * @str: to create a copy
+ *
+ * Return: copy from str
+ */
+
+char	*_strdup(char *str)
+{
+	char	*s;
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	s = malloc(i + 1);
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,5 @@
 # include <unistd.h>
 
 char	*create_array(unsigned int size, char c);
-
+char	*_strdup(char *str);
 #endif
