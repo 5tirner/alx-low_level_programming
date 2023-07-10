8c4dd5d3efb19536a7cc408d7c7773a7c03154c1
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-09 23:37:46 -0700

    strjoin

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * len - calcul len of s
+ * @s: string
+ *
+ * Return: len
+ */
+
+int	len(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * join - join two str
+ * @s1: s1
+ * @s2: s2
+ * @s: string wanted
+ *
+ * Return: string
+ */
+
+char	*join(char *s, char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		s[j] = s1[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		s[j] = s2[i];
+		i++;
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
+
+/**
+ * str_concat - join two string
+ * @s1: str1
+ * @s2: str2
+ *
+ * Return: pointer to char
+ */
+
+char	*str_concat(char *s1, char *s2)
+{
+	char	*s;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (s2);
+	if (!s2)
+		return (s1);
+	s = malloc(len(s1) + len(s2) + 1);
+	if (!s)
+		return (NULL);
+	s = join(s, s1, s2);
+	return (s);
+}
--- 0x0B-malloc_free/nain.c
+++ 0x0B-malloc_free/nain.c
@@ -7,14 +7,19 @@
  *
  * Return: Always 0.
  */
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     char *s;
 
-    s = _strdup("ALX SE");
+    s = str_concat("Betty ", "Holberton");
     if (s == NULL)
     {
-        printf("failed to allocate memory\n");
+        printf("failed\n");
         return (1);
     }
     printf("%s\n", s);
