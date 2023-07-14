9169f0f2988807953429c027f9fd7c4c7f971be9
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-14 02:56:50 -0700

    calloc func

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _calloc - recode for calloc function
+ * @nmemb: count
+ * @size: size to allocate
+ *
+ * Return: an address
+ */
+
+void	*_calloc(unsigned int nmemb, unsigned int size)
+{
+	void		*address;
+	char		*s;
+	unsigned long	i;
+
+	if (!nmemb || !size)
+		return (NULL);
+	if (nmemb > SIZE_MAX / size)
+		return (NULL);
+	address = malloc(nmemb * size);
+	s = (char *)address;
+	i = 0;
+	while (i < nmemb * size)
+	{
+		s[i] = 0;
+		i++;
+	}
+	return (address);
+}
