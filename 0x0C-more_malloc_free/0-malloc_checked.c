912857f21494faf255faac5b324c3ef33a99ea28
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-14 02:09:42 -0700

    allocation size b

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - to allocate b size
+ * b: unsigned int
+ *
+ * Return: pointer
+ */
+
+void	*malloc_checked(unsigned int b)
+{
+	void	*pointer;
+
+	pointer = malloc(b);
+	if (!pointer)
+		exit (98);
+	return (pointer);
+}
+
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void	*malloc_checked(unsigned int b);
+
+#endif
