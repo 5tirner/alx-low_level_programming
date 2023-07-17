eec499a8faa53da92c4834694138539eecccd220
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-17 13:07:29 -0700

    dog initialize with address

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dogs - initialize valeus in struct elements
+ * @d: struct
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ * Return: NoThing
+ */
+
+void	init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
