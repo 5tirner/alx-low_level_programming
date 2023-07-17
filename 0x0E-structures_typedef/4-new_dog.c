25049cfab4569d7a1992d0c6cb93a55c4ac16cbf
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-17 15:28:12 -0700

    modify

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,6 +50,10 @@ dog_t	*new_dog(char *name, float age, char *owner)
 	ow = dup_str(owner);
 	if (!na || !ow)
 	{
+		if (na)
+			free(na);
+		if (ow)
+			free(ow);
 		free(new);
 		return (NULL);
 	}
