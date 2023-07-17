2b98fee9ebd03ecbd2d96f5c3879e8d87d2e3395
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-17 15:07:31 -0700

    modify

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,34 @@
 #include "dog.h"
 
 /**
+ * dup_str - duplicate str
+ * @str: string
+ *
+ * Return: NULL or address
+ */
+
+char	*dup_str(char *str)
+{
+	int	i;
+	char	*s;
+
+	i = 0;
+	while (str[i])
+		i++;
+	s = malloc(i + 1);
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
+
+/**
  * new_dog - create a new dog
  * @name: dog name
  * @age: dog age
@@ -12,12 +40,21 @@
 dog_t	*new_dog(char *name, float age, char *owner)
 {
 	dog_t	*new;
+	char	*na;
+	char	*ow;
 
 	new = malloc(sizeof(dog_t));
 	if (!new)
 		return (NULL);
-	new->name = name;
+	na = dup_str(name);
+	ow = dup_str(owner);
+	if (!na || !ow)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->name = na;
 	new->age = age;
-	new->owner = owner;
+	new->owner = ow;
 	return (new);
 }
