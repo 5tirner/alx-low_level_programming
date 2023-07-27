500adbd9b57fec57dd4548766a0f0897d95ce00a
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-27 08:42:24 -0700

    add a node in the front of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_node - add in the front
+ * @head: the list
+ * @str: value
+ *
+ * Return: the list with a node in it's fornt
+ */
+
+list_t	*add_node(list_t **head, const char *str)
+{
+	list_t	*ptr;
+
+	ptr = malloc(sizeof(list_t));
+	ptr->str = (char *)str;
+	ptr->next = *head;
+	(*head) = ptr;
+	return (*head);
+}
