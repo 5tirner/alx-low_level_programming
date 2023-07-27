78f7477ba211b8cd6721592b9a0448c6e81b9a41
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-27 08:50:06 -0700

    add in the front of a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,13 +11,21 @@
 list_t	*add_node(list_t **head, const char *str)
 {
 	list_t	*ptr;
+	int	i;
 
 	ptr = malloc(sizeof(list_t));
 	if (!ptr)
 		return (NULL);
 	ptr->str = strdup(str);
 	if (!ptr->str)
+	{
+		ptr->len = 0;
 		return (NULL);
+	}
+	i = 0;
+	while (str[i])
+		i++;
+	ptr->len = i;
 	ptr->next = *head;
 	(*head) = ptr;
 	return (ptr);
