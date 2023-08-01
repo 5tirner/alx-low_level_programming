93a821a6f7c198cf9da4db654b2975a2bd87d6b6
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-08-01 12:30:19 -0700

    free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a linked list
+ * @head: the list
+ *
+ * Return: NoThing
+ */
+
+void	free_listint2(listint_t **head)
+{
+	listint_t	*ptr;
+
+	while (*head)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+	*head = NULL;
+}
