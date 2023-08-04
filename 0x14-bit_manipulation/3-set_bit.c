76a1f9dedcc02a891a493968fab469294a1f99c3
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-08-03 18:27:32 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - set val in bit by index
+ * @n: the number
+ * @inedx: the index
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int	set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * sizeof(unsigned long int *))
+		return (-1);
+	*n ^= (1 << index);
+	return (1);
+}
