717b21838bcff35a648d2b86b8496d3981716ea4
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-08-03 19:08:03 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,27 @@
 
 void	print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_pc((n & 1) + 48);
+	int			r;
+	unsigned long int	max;
+	unsigned long int	i;
+
+	max = ULLONG_MAX;
+	r = 0;
+	while (max != 0)
+	{
+		i = max;
+		if (i < ULLONG_MAX)
+			i++;
+		if (n >= i)
+		{
+			write(1, "1", 1);
+			r = 1;
+			n -= i;
+		}
+		else if (r == 1)
+			write(1, "0", 1);
+		max /= 2;
+	}
+	n += 48;
+	write(1, &n, 1);
 }
