16d4bd065860da267ff4966e2883e76067a71ed6
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-08-09 10:39:12 -0700

    create a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
  * @filename: file ro read it
  * @letters: ho much read from the file
  *
- * Return: what read get or 0 0 if !filename
+ * Return: what read get or 0 if !filename
  */
 
 ssize_t	read_textfile(const char *filename, size_t letters)
@@ -17,13 +17,13 @@ ssize_t	read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDWR, 0777);
+	fd = open(filename, O_RDONLY, 0777);
 	if (fd == -1)
 		return (0);
-	i = 0;
 	n = read(fd, &c, 1);
 	if (n == -1)
 		return (0);
+	i = 0;
 	while (i < letters && n != 0)
 	{
 		if (write(1, &c, 1) == -1)
