48f6b3bce3f0496c6989b288b98b6daa00203ba0
Zakaria Sabri <zasabri@c3r6p4.1337.ma>
2023-09-28 17:26:40 +0100

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert from binary to unsigned int
+ * @b: Binary NUmber
+ *
+ * Return: the number or NULL if SMTW.
+ */
+
+unsigned int	binary_to_uint(const char *b)
+{
+	int				i;
+	unsigned long	until;
+	unsigned int	number;
+
+	i = 0;
+	if (!b || !b[i])
+		return (0);
+	while (b[i])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+	until = 1;
+	number = 0;
+	while (b[--i])
+	{
+		if (b[i] == '1')
+			number += until;
+		until *= 2;
+	}
+	return (number);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+unsigned int	binary_to_uint(const char *b);
+void			print_binary(unsigned long int n);
+
+#endif
