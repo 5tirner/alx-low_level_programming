f1851549533f2d621f3ec3da852e4f733d616eca
5tirner <thatfor1337@gmail.com>
2023-09-28 20:45:36 +0100

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,19 +15,3 @@ int set_bit(unsigned long int *n, unsigned int index)
 	*n ^= 1 << index;
 	return (1);
 }
-
-int main(void)
-{
-    unsigned long int n;
-
-    n = 1024;
-    set_bit(&n, 5);
-    printf("%lu\n", n);
-    n = 0;
-    set_bit(&n, 10);
-    printf("%lu\n", n);
-    n = 98;
-    set_bit(&n, 0);
-    printf("%lu\n", n);
-    return (0);
-}
