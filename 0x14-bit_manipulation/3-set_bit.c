6d489479e5bf2b7f60ccf2a64d02f522674140f6
Zakaria Sabri <zasabri@c3r6p4.1337.ma>
2023-09-28 20:40:59 +0100

    setbit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bet - set 1 into a bit
+ * set_bit - set 1 into a bit
  * @n: the number
  * @index: the index to set on it
  *
@@ -14,4 +14,20 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	*n ^= 1 << index;
 	return (1);
+}
+
+int main(void)
+{
+    unsigned long int n;
+
+    n = 1024;
+    set_bit(&n, 5);
+    printf("%lu\n", n);
+    n = 0;
+    set_bit(&n, 10);
+    printf("%lu\n", n);
+    n = 98;
+    set_bit(&n, 0);
+    printf("%lu\n", n);
+    return (0);
 }
