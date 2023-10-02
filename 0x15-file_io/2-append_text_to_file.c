b3e3394247bdff737ffff3baa8c24770329cffc6
root <thatfor1337@gmail.com>
2023-10-02 13:43:42 -0700

    append str to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append a str into a file
+ * @filename: the name of the file
+ * @text_content: the string to add
+ *
+ * Return: 1 if all is good or -1 case of error
+ */
+
+int	append_text_to_file(const char *filename, char *text_content)
+{
+	int	fd;
+	int	i;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		return (1);
+	i = 0;
+	while (text_content[i])
+	{
+		if (write(1, &text_content[i], 1) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		i++;
+	}
+	return (1);
+}
