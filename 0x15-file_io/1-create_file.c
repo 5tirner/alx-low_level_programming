eed41523ffd266a8c54df41dbe42336abf395e69
root <thatfor1337@gmail.com>
2023-10-03 14:12:00 -0700

    fix again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int	create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_TRUNC);
+	fd = open(filename, O_RDWR | O_TRUNC);
 	if (fd == -1)
 		fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fd == -1)
